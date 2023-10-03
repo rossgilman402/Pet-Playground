ef0c8ede3e88bbe40d76f9d88948989fc8f3cef6
ross402 <65184393+ross402@users.noreply.github.com>
2023-10-03 11:33:00 -0400

    Added path import to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const express = require("express");
 const session = require("express-session");
+const path = require("path");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
