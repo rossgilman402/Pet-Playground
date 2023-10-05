77599d1ce82dacefe7a2716c14daeda224688710
mhorton <horton-michael@users.noreply.github.com>
2023-10-05 11:04:14 -0400

    imports multer upload engine

--- controllers/api/petPostRoutes.js
+++ controllers/api/petPostRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { Post } = require("../../models");
 const withAuth = require("../../utils/auth");
+const upload = require("../../server").upload;
 
 //api/pet-post/
 
