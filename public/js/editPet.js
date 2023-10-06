a04b60bae6266995d4b2026e6894c4d259d0611b
MaddyKm <134648462+MaddyKm@users.noreply.github.com>
2023-10-06 11:17:15 -0400

    adds edit pet form to profile page

---
+++ public/js/editPet.js
@@ -0,0 +1,8 @@
+function editPetForm() {
+  var x = document.getElementById("editPetEl");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
